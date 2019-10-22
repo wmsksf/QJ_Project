3dd0102be4090a7d7848561e054e55ba2ce29dcb
wmsksf <adamantiasofi@gmail.com>
2019-10-22 22:06:01 +0300

    quicksort (and few data types)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "We need credits!" << std::endl;
+int main(int argc, char **argv)
+{
+
     return 0;
 }
