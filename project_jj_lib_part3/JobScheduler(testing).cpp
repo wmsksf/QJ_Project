281ea1201edac8132d83378962709027a4212b88
wmsksf <adamantiasofi@gmail.com>
2020-01-13 01:29:35 +0200

    job scheduling more promising (still testing)

--- project_jj_lib_part3/JobScheduler(testing).cpp
+++ project_jj_lib_part3/JobScheduler(testing).cpp
@@ -2,22 +2,14 @@
 // Created by wmsksf on 12/01/20.
 //
 
-#include <iostream>
-#include <unistd.h>
-#include <vector>
-#include <pthread.h>
-
 //TESTING
 #include <iostream>
+#include <queue>
 #include <unistd.h>
-#include <vector>
 #include <pthread.h>
+#include <vector>
 
 #include <sstream>
-/** Thread safe cout class
-  * Exemple of use:
-  *    s_cout{} << "Hello world!" << std::endl;
-  */
 class s_cout: public std::ostringstream
 {
 public:
@@ -38,7 +30,7 @@ pthread_mutex_t s_cout::s_mux{};
 class Job {
 public:
     virtual void run() = 0;
-    virtual ~Job() {}
+    virtual ~Job() { }
 };
 
 class JobScheduler {
@@ -46,61 +38,66 @@ public:
     JobScheduler();
     ~JobScheduler();
 
-    void schedule(Job &job);
-    void barrier();
-
-    pthread_mutex_t WorkerMux;
-    pthread_cond_t WorkerCond;
+    void schedule(Job &Job);
 
     const int ThreadPoolSize;
     std::vector<pthread_t> ThreadPool;
     std::vector<Job*> JobsReadyToRunQueue;
+    std::queue<Job*> allJobsQueue;
+
+    pthread_t ManagerThr;
+    pthread_mutex_t ManagerMtx, WorkerMtx;
+    pthread_cond_t ManagerCV, WorkerCV;
+
+    bool WorkerIsRunning;
+    bool ManagerIsRunning;
 };
 
 void* worker_thread(void*);
+void* manager_thread(void*);
 
-// get max #threads of h/w
-JobScheduler::JobScheduler() : ThreadPoolSize{4}
+JobScheduler::JobScheduler() : ThreadPoolSize{16}, WorkerIsRunning{true}, ManagerIsRunning{true}
 {
     pthread_t thread;
     for (int i = 0; i <ThreadPoolSize; ++i) {
         pthread_create(&thread,nullptr,worker_thread, (void*)this);
         ThreadPool.push_back(thread);
     }
+    pthread_create(&ManagerThr,nullptr,manager_thread, (void*)this);
 
-    pthread_mutex_init(&WorkerMux,nullptr);
-    pthread_cond_init(&WorkerCond,nullptr);
+    pthread_mutex_init(&WorkerMtx,nullptr);
+    pthread_cond_init(&WorkerCV,nullptr);
+    pthread_mutex_init(&ManagerMtx,nullptr);
+    pthread_cond_init(&ManagerCV,nullptr);
 }
 
 JobScheduler::~JobScheduler()
 {
     s_cout{} << "~JobScheduler start\n";
-    pthread_mutex_lock(&WorkerMux);
-    // in barrier!
-    // in order to wake up waiting threads so as cond to be destroyed
-    // pthread_cond_broadcast(&WorkerCond);
+    pthread_mutex_lock(&ManagerMtx);
+    ManagerIsRunning = false;
+    pthread_cond_signal(&ManagerCV);
+    pthread_mutex_unlock(&ManagerMtx);
+    pthread_join(ManagerThr, nullptr);
+    s_cout{} << "cleaning of manager done\n";
+    s_cout{} << "cleaning of workers...\n";
+    pthread_mutex_lock(&WorkerMtx);
+    WorkerIsRunning = false;
+    pthread_cond_broadcast(&WorkerCV);
+    s_cout{} << "broadcast...\n";
+    pthread_mutex_unlock(&WorkerMtx);
+    s_cout{} << "join...\n";
     for (auto &t : ThreadPool)
         pthread_join(t, nullptr);
-    pthread_cond_destroy(&WorkerCond);
-
-    pthread_mutex_unlock(&WorkerMux);
-    pthread_mutex_destroy(&WorkerMux);
-    std::cout << "~JobScheduler end\n";
+    s_cout{} << "~JobScheduler end\n";
 }
 
 void JobScheduler::schedule(Job &Job)
 {
-    pthread_mutex_lock(&WorkerMux);
-
-    JobsReadyToRunQueue.push_back(&Job);
-    pthread_cond_signal(&WorkerCond);
-
-    pthread_mutex_unlock(&WorkerMux);
-}
-
-void JobScheduler::barrier()
-{
-
+    pthread_mutex_lock(&ManagerMtx);
+    allJobsQueue.push(&Job);
+    pthread_cond_signal(&ManagerCV);
+    pthread_mutex_unlock(&ManagerMtx);
 }
 
 void* worker_thread(void *arg)
@@ -108,19 +105,48 @@ void* worker_thread(void *arg)
     auto* scheduler = (JobScheduler*)arg;
     for (;;)
     {
-        pthread_mutex_lock(&(scheduler->WorkerMux));
+        pthread_mutex_lock(&(scheduler->WorkerMtx));
 
-        while(!scheduler->JobsReadyToRunQueue.size())
-        {
-            pthread_cond_wait(&(scheduler->WorkerCond), &(scheduler->WorkerMux));
+        while (!scheduler->JobsReadyToRunQueue.size() && scheduler->WorkerIsRunning) {
+            pthread_cond_wait(&(scheduler->WorkerCV), &(scheduler->WorkerMtx));
         }
-        Job *p = scheduler->JobsReadyToRunQueue.front();
-        scheduler->JobsReadyToRunQueue.erase(scheduler->JobsReadyToRunQueue.begin());
+        if (!scheduler->WorkerIsRunning) {
+            pthread_mutex_unlock(&(scheduler->WorkerMtx));
+            break;
+        }
 
-        pthread_mutex_unlock(&(scheduler->WorkerMux));
+        Job *p = scheduler->JobsReadyToRunQueue.back();
+        scheduler->JobsReadyToRunQueue.pop_back();
+        pthread_mutex_unlock(&(scheduler->WorkerMtx));
 
         p->run();
         delete p;
+    }
+}
+
+void* manager_thread(void *arg)
+{
+    auto* scheduler = (JobScheduler*)arg;
+    for (;;)
+    {
+        pthread_mutex_lock(&(scheduler->ManagerMtx));
+        if (!scheduler->ManagerIsRunning) {
+            pthread_mutex_unlock(&(scheduler->ManagerMtx));
+            break;
+        }
+
+        if (scheduler->allJobsQueue.size() != 0)
+        {
+            Job *p = scheduler->allJobsQueue.front();
+
+            pthread_mutex_lock(&(scheduler->WorkerMtx));
+            scheduler->JobsReadyToRunQueue.push_back(p);
+            pthread_cond_signal(&(scheduler->WorkerCV));
+            pthread_mutex_unlock(&(scheduler->WorkerMtx));
+
+            scheduler->allJobsQueue.pop();
+        }
+        pthread_mutex_unlock(&(scheduler->ManagerMtx));
     }
 }
 
@@ -130,15 +156,15 @@ public:
     job(int n=0) : n{n} { }
     void run() override
     {
-        s_cout{} << "Start of job " << n << std::endl;;
+        s_cout{} << "Start Job " << n << std::endl;;
         sleep(2);
-        s_cout{} << "End of job " << n << std::endl;;
+        s_cout{} << " Stop Job " << n << std::endl;;
     }
 };
 
 int main()
 {
-    JobScheduler scheduler;
+    JobScheduler sched;
 
     Job *t0 = new job{0};
     Job *t1 = new job{1};
@@ -147,12 +173,12 @@ int main()
     Job *t4 = new job{4};
     Job *t5 = new job{5};
 
-    scheduler.schedule(*t0);
-    scheduler.schedule(*t1);
-    scheduler.schedule(*t2);
-    scheduler.schedule(*t3);
-    scheduler.schedule(*t4);
-    scheduler.schedule(*t5);
+    sched.schedule(*t0);
+    sched.schedule(*t1);
+    sched.schedule(*t2);
+    sched.schedule(*t3);
+    sched.schedule(*t4);
+    sched.schedule(*t5);
 
-    sleep(20);
+    sleep(10);
 }
