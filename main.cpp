ce7c33abef128e9b4e098a9f7389c16ac799dd9c
wmsksf <adamantiasofi@gmail.com>
2020-01-14 17:14:20 +0200

    JobScheduler

--- main.cpp
+++ main.cpp
@@ -1,16 +1,47 @@
-#include <iostream>
-#include <climits>
-#include <cstring>
-#include "project_jj_lib/Utils.h"
-#include "project_jj_lib_part2/Utils_part2.h"
-#include "project_jj_lib_part2/MACROS.h"
+//#include <iostream>
+//#include <climits>
+//#include <cstring>
+//#include "project_jj_lib/Utils.h"
+//#include "project_jj_lib_part2/Utils_part2.h"
+//#include "project_jj_lib_part2/MACROS.h"
+//
+//using namespace std;
+//
+//int main(int argc, char **argv)
+//{
+//    Get_Input();
+//    Set_output();
+//
+//    return 0;
+//}
 
-using namespace std;
+#include <cstdlib>
+#include "project_jj_lib_part3/JobScheduler.h"
+
 
 int main(int argc, char **argv)
 {
-    Get_Input();
-    Set_output();
+    if (argc < 2) {
+        s_cout{} << "give #threads...\n";
+        exit(1);
+    }
+    JobScheduler sched;
+    sched.init(atoi(argv[1]));
 
-    return 0;
+    Job *t0 = new job{0};
+    Job *t1 = new job{1};
+    Job *t2 = new job{2};
+    Job *t3 = new job{3};
+    Job *t4 = new job{4};
+    Job *t5 = new job{5};
+
+    sched.schedule(*t0);
+    sched.schedule(*t1);
+    sched.schedule(*t2);
+    sched.schedule(*t3);
+    sched.schedule(*t4);
+    sched.schedule(*t5);
+
+    // sleep(10);
+    sched.stop();
 }
---
+++ project_jj_lib_part3/JobScheduler.h
@@ -0,0 +1,126 @@
+//
+// Created by wmsksf on 14/01/20.
+//
+
+#ifndef PROJECT_JJ_JOBSCHEDULER_H
+#define PROJECT_JJ_JOBSCHEDULER_H
+
+#include <pthread.h>
+#include <unistd.h>
+#include <sstream>
+#include <iostream>
+#include <queue>
+
+class Job {
+public:
+    virtual void run() = 0;
+    virtual ~Job() { }
+};
+
+class JobScheduler {
+public:
+    JobScheduler();
+    ~JobScheduler();
+
+    void init(int threads);
+    void stop();
+
+    void schedule(Job &Job);
+    void barrier();
+
+    int ThreadPoolSize;
+    std::vector<pthread_t> ThreadPool;
+//    std::vector<bool> ThreadPoolIdle;
+
+    std::queue<Job*> JobsQueue;
+
+    pthread_mutex_t WorkerMtx, IdleMtx;
+    pthread_cond_t WorkerCV, IdleCV;
+
+    bool WorkerIsRunning;
+};
+
+void* worker_thread(void*);
+
+class job : public Job {
+    int n;
+public:
+    job(int n=0);
+    void run() override;
+};
+//----------------------------------------------------------------------------------------------------------------------
+class arguments {
+public:
+    virtual ~arguments() {}
+};
+
+class args : public arguments {
+public:
+    JobScheduler *scheduler;
+    int i;
+
+    args();
+    void set(JobScheduler *scheduler, int i);
+};
+
+class s_cout: public std::ostringstream
+{
+public:
+    s_cout() = default;
+    ~s_cout() override;
+
+private:
+    static pthread_mutex_t s_mux;
+};
+
+// from Little Book of Semaphores
+// typedef struct {
+//   int n;
+//   int count;
+//   sem_t mutex;
+//   sem_t turnstile;
+//   sem_t turnstile2;
+// } barrier_t;
+
+// void init_barrier(barrier_t *barrier, int n)
+// {
+//   barrier->n = n;
+//   barrier->count = 0;
+//   sem_init(&barrier->mutex, 0, 1);
+//   sem_init(&barrier->turnstile, 0, 0);
+//   sem_init(&barrier->turnstile2, 0, 0);
+// }
+
+// void phase1_barrier(barrier_t *barrier)
+// {
+//   sem_wait(&barrier->mutex);
+//   if (++barrier->count == barrier->n) {
+//     int i;
+//     for (i = 0; i < barrier->n; i++) {
+//       sem_post(&barrier->turnstile);
+//     }
+//   }
+//   sem_post(&barrier->mutex);
+//   sem_wait(&barrier->turnstile);
+// }
+
+// void phase2_barrier(barrier_t *barrier)
+// {
+//   sem_wait(&barrier->mutex);
+//   if (--barrier->count == 0) {
+//     int i;
+//     for (i = 0; i < barrier->n; i++) {
+//       sem_post(&barrier->turnstile2);
+//     }
+//   }
+//   sem_post(&barrier->mutex);
+//   sem_wait(&barrier->turnstile2);
+// }
+
+// void wait_barrier(barrier_t *barrier)
+// {
+//   phase1_barrier(barrier);
+//   phase2_barrier(barrier);
+// }
+
+#endif //PROJECT_JJ_JOBSCHEDULER_H
